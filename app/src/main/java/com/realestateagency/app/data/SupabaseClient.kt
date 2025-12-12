9473e09046f3b130824eafc57f70df2578a46397
p3nance <p3nancezxc@gmail.com>
2025-12-12 22:13:13 +0400

    удален лишний импорт библиотеки

--- app/src/main/java/com/realestateagency/app/data/SupabaseClient.kt
+++ app/src/main/java/com/realestateagency/app/data/SupabaseClient.kt
@@ -3,7 +3,6 @@ package com.realestateagency.app.data
 import com.realestateagency.app.Config
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
-import io.github.jan.supabase.postgrest.postgrest
 
 val supabaseClient = createSupabaseClient(
     supabaseUrl = Config.SUPABASE_URL,
